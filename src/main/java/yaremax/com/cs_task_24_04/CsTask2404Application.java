28db55d4476fc29c18f6ba73bdb84ba8438700cc
Yarema Maksym <macsym2@gmail.com>
2024-04-25 20:02:26 +0300

    init project

---
+++ src/main/java/yaremax/com/cs_task_24_04/CsTask2404Application.java
@@ -0,0 +1,13 @@
+package yaremax.com.cs_task_24_04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsTask2404Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsTask2404Application.class, args);
+    }
+
+}
---
+++ src/test/java/yaremax/com/cs_task_24_04/CsTask2404ApplicationTests.java
@@ -0,0 +1,13 @@
+package yaremax.com.cs_task_24_04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsTask2404ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
