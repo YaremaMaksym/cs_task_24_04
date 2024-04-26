b462e62071d168667cced644e1834a1b720b46a9
Yarema Maksym <macsym2@gmail.com>
2024-04-26 15:02:06 +0300

    feat: add user model

---
+++ src/main/java/yaremax/com/cs_task_24_04/user/User.java
@@ -0,0 +1,36 @@
+package yaremax.com.cs_task_24_04.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private LocalDate birthDate;
+
+    private String address;
+    private String phone;
+}
