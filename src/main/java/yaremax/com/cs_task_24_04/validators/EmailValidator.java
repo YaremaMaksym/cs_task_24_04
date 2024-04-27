b08234a980b776c817d6b894ee437081f0ad181d
Maksym Yarema <macsym2@gmail.com>
2024-04-27 08:47:16 +0300

    fix: "-" and "_" now not allowed in email username

--- src/main/java/yaremax/com/cs_task_24_04/validators/EmailValidator.java
+++ src/main/java/yaremax/com/cs_task_24_04/validators/EmailValidator.java
@@ -21,7 +21,7 @@ public class EmailValidator {
     user_name@domain.com
     username@.com
      */
-    private static final String EMAIL_REGEX_PATTERN = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@"
+    private static final String EMAIL_REGEX_PATTERN = "^(?=.{1,64}@)[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*@"
             + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
 
     public boolean isValid(String email) {
